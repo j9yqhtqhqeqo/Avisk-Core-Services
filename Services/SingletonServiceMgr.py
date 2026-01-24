9b0dae7672c71c81cd8f69d00716a73077165894
j9yqhtqhqeqo <mohan.ganadal@gmail.com>
2026-01-24 12:04:52 -0500

    commit2

--- Services/SingletonServiceMgr.py
+++ Services/SingletonServiceMgr.py
@@ -1,3 +1,12 @@
+from Utilities.MultiProcessing import get_process_buffer
+from Utilities.Lookups import Lookups
+import multiprocessing
+from DBEntities.LookupsDBManager import LookupsDBManager
+import time
+from multiprocessing import Process, Queue, Pool
+from DBEntities.ProximityEntity import DocumentEntity
+from Services.InsightGenerator import file_folder_keyWordSearchManager, PARM_STAGE1_FOLDER
+from DBEntities.InsightGeneratorDBManager import InsightGeneratorDBManager
 import sys
 from pathlib import Path
 import warnings
@@ -5,22 +14,10 @@ import logging
 
 # Suppress Streamlit ScriptRunContext warnings
 warnings.filterwarnings('ignore', message='.*ScriptRunContext.*')
-logging.getLogger('streamlit.runtime.scriptrunner.script_runner').setLevel(logging.ERROR)
+logging.getLogger(
+    'streamlit.runtime.scriptrunner.script_runner').setLevel(logging.ERROR)
 
 sys.path.append(str(Path(sys.argv[0]).resolve().parent.parent))
-
-from DBEntities.InsightGeneratorDBManager import InsightGeneratorDBManager
-
-from Services.InsightGenerator import file_folder_keyWordSearchManager, PARM_STAGE1_FOLDER
-from DBEntities.ProximityEntity import DocumentEntity
-from multiprocessing import Process, Queue, Pool
-import time
-from DBEntities.LookupsDBManager import LookupsDBManager
-import multiprocessing
-from Utilities.Lookups import Lookups
-from Utilities.MultiProcessing import get_process_buffer
-
-
 
 
 def load_document_cache_for_exposure_pathway(database_context, validation_mode, queue: Queue, queue_size=Queue):
@@ -30,25 +27,29 @@ def load_document_cache_for_exposure_pathway(database_context, validation_mode, 
         queue.put(document)
 
     queue_size.put(len(exposure_document_list))
-    print('Documents Loaded for Exposure Pathway Keyword Search:', len(exposure_document_list))
+    print('Documents Loaded for Exposure Pathway Keyword Search:',
+          len(exposure_document_list))
 
-def process_next_unprocessed_exposure_document_list(batch_size, database_context, queue: Queue, batch_num, validation_mode ):
+
+def process_next_unprocessed_exposure_document_list(batch_size, database_context, queue: Queue, batch_num, validation_mode):
     Document_List = []
     for i in range(batch_size):
         if (queue.empty()):
             break
         document: DocumentEntity = queue.get()
         Document_List.append(document)
-        
+
     key_word_search_mgr = file_folder_keyWordSearchManager(
         folder_path=PARM_STAGE1_FOLDER, database_context=database_context)
-    
+
     key_word_search_mgr.generate_keyword_location_map_for_exposure_pathway(
         Document_List, batch_num, validation_mode)
-    if(validation_mode):
-        InsightGeneratorDBManager(database_context).update_validation_keywords_generated_status(Document_List, Lookups().Exposure_Pathway_Dictionary_Type,2)
+    if (validation_mode):
+        InsightGeneratorDBManager(database_context).update_validation_keywords_generated_status(
+            Document_List, Lookups().Exposure_Pathway_Dictionary_Type, 2)
 
-def process_exposure_pathway_document_list(database_context, validation_mode = False):
+
+def process_exposure_pathway_document_list(database_context, validation_mode=False):
     print("Creating Batches for Exposure Pathway Keyword Search -")
     queue = Queue()
     queue_size = Queue()
@@ -68,14 +69,15 @@ def process_exposure_pathway_document_list(database_context, validation_mode = F
         # Check if the batch is set to run, if not exit
         l_dbmgr = LookupsDBManager(database_context=database_context)
         process_state = (l_dbmgr.get_exposure_pathway_search_status())
-        if(process_state == 'Run'):
+        if (process_state == 'Run'):
             p = (Process(target=process_next_unprocessed_exposure_document_list,
-                args=(batches[i], database_context, queue, i+1,validation_mode,)))
+                         args=(batches[i], database_context, queue, i+1, validation_mode,)))
             p.start()
             process_list.append(p)
             print('Started Batch: ' + str(i+1))
         else:
-            print('Process Not in Run Stat - Exiting process_exposure_pathway_document_list')
+            print(
+                'Process Not in Run Stat - Exiting process_exposure_pathway_document_list')
             break
 
     cache_loader.join()
@@ -94,6 +96,7 @@ def load_document_cache_for_internalization(database_context, validation_mode, q
     queue_size.put(len(internalization_document_list))
     print('Documents Loaded for Internalization Keyword Search')
 
+
 def process_next_unprocessed_internalization_document_list(batch_size, database_context, queue: Queue, batch_num, validation_mode):
     Document_List = []
     for i in range(batch_size):
@@ -101,16 +104,18 @@ def process_next_unprocessed_internalization_document_list(batch_size, database_
             break
         document: DocumentEntity = queue.get()
         Document_List.append(document)
-        
+
     key_word_search_mgr = file_folder_keyWordSearchManager(
         folder_path=PARM_STAGE1_FOLDER, database_context=database_context)
-    
-    key_word_search_mgr.generate_keyword_location_map_for_internalization(
-       Document_List, batch_num, validation_mode)
-    if(validation_mode):
-        InsightGeneratorDBManager(database_context).update_validation_keywords_generated_status(Document_List, Lookups().Internalization_Dictionary_Type,2)
 
-def process_internalization_document_list(database_context, validation_mode = False):
+    key_word_search_mgr.generate_keyword_location_map_for_internalization(
+        Document_List, batch_num, validation_mode)
+    if (validation_mode):
+        InsightGeneratorDBManager(database_context).update_validation_keywords_generated_status(
+            Document_List, Lookups().Internalization_Dictionary_Type, 2)
+
+
+def process_internalization_document_list(database_context, validation_mode=False):
     print("Creating Batches for Internalization Keyword Search - ")
 
     queue = Queue()
@@ -131,20 +136,21 @@ def process_internalization_document_list(database_context, validation_mode = Fa
         # Check if the batch is set to run, if not exit
         l_dbmgr = LookupsDBManager(database_context=database_context)
         process_state = (l_dbmgr.get_exposure_pathway_search_status())
-        if(process_state == 'Run'):
+        if (process_state == 'Run'):
             p = (Process(target=process_next_unprocessed_internalization_document_list,
-                args=(batches[i], database_context, queue, i+1,validation_mode,)))
+                         args=(batches[i], database_context, queue, i+1, validation_mode,)))
             p.start()
             process_list.append(p)
             print('Started Batch: ' + str(i+1))
         else:
-            print('Process Not in Run Stat - Exiting process_exposure_pathway_document_list')
+            print(
+                'Process Not in Run Stat - Exiting process_exposure_pathway_document_list')
             break
 
     cache_loader.join()
     for process in process_list:
         process.join()
-    
+
     print('All documents processed:Check for documents failed keyword validation')
 
 
@@ -156,7 +162,8 @@ def load_document_cache_for_mitigation(database_context, validation_mode, queue_
 
     queue_size_mit.put(len(mitigation_document_list))
     print('Documents Loaded for Mitigation Keyword Search')
-  
+
+
 def process_next_unprocessed_mitigation_document_list(batch_size, database_context, queue: Queue, batch_num, validation_mode):
     Document_List = []
 
@@ -168,13 +175,15 @@ def process_next_unprocessed_mitigation_document_list(batch_size, database_conte
 
     key_word_search_mgr = file_folder_keyWordSearchManager(
         folder_path=PARM_STAGE1_FOLDER, database_context=database_context)
-    
-    key_word_search_mgr.generate_keyword_location_map_for_mitigation(
-       Document_List, batch_num, validation_mode)
-    if(validation_mode):
-        InsightGeneratorDBManager(database_context).update_validation_keywords_generated_status(Document_List, Lookups().Mitigation_Dictionary_Type,2)
 
-def process_mitigation_document_list(database_context, validation_mode = False):
+    key_word_search_mgr.generate_keyword_location_map_for_mitigation(
+        Document_List, batch_num, validation_mode)
+    if (validation_mode):
+        InsightGeneratorDBManager(database_context).update_validation_keywords_generated_status(
+            Document_List, Lookups().Mitigation_Dictionary_Type, 2)
+
+
+def process_mitigation_document_list(database_context, validation_mode=False):
     print("Creating Batches for Mitigation Keyword Search -")
 
     queue = Queue()
@@ -186,7 +195,7 @@ def process_mitigation_document_list(database_context, validation_mode = False):
 
     queue_size_int = queue_size.get()
 
-    batches = get_process_buffer(queue_size_int,True)
+    batches = get_process_buffer(queue_size_int, True)
     num_batches = len(batches)
     print("Number of Documents to Process:" + str(queue_size_int))
     print("Total Number of Batches:" + str(num_batches))
@@ -196,21 +205,23 @@ def process_mitigation_document_list(database_context, validation_mode = False):
         # Check if the batch is set to run, if not exit
         l_dbmgr = LookupsDBManager(database_context=database_context)
         process_state = (l_dbmgr.get_exposure_pathway_search_status())
-        if(process_state == 'Run'):
+        if (process_state == 'Run'):
             p = (Process(target=process_next_unprocessed_mitigation_document_list,
-                args=(batches[i], database_context, queue, i+1,validation_mode,)))
+                         args=(batches[i], database_context, queue, i+1, validation_mode,)))
             p.start()
             process_list.append(p)
             print('Started Batch: ' + str(i+1))
         else:
-            print('Process Not in Run Stat - Exiting process_exposure_pathway_document_list')
+            print(
+                'Process Not in Run Stat - Exiting process_exposure_pathway_document_list')
             break
 
     cache_loader.join()
     for process in process_list:
         process.join()
-    
+
     print('All documents processed:Check for documents failed keyword validation')
+
 
 def update_validation_completed_status(database_context):
     # print("✅ Processed Dictionary Terms Successfully - DEBUG -- SINGLETON")
@@ -221,18 +232,17 @@ def update_validation_completed_status(database_context):
 
 def update_sector_stats(database_context, sector, year: int, generate_exp_sector_insights: bool, generate_int_sector_insights: bool, generate_exp_mit_sector_insights: bool, generate_exp_int_mit_sector_insights: bool, update_all: bool):
     InsightGeneratorDBManager(
-        database_context).update_sector_stats(sector, year, generate_exp_sector_insights, generate_int_sector_insights,generate_exp_mit_sector_insights, generate_exp_int_mit_sector_insights, update_all)
+        database_context).update_sector_stats(sector, year, generate_exp_sector_insights, generate_int_sector_insights, generate_exp_mit_sector_insights, generate_exp_int_mit_sector_insights, update_all)
 
 
-def update_reporting_tables(database_context, sector, year: int, generate_exp_sector_insights: bool, generate_int_sector_insights: bool, generate_mit_sector_insights: bool, update_all: bool, keywords_only:bool):
+def update_reporting_tables(database_context, sector, year: int, generate_exp_sector_insights: bool, generate_int_sector_insights: bool, generate_mit_sector_insights: bool, update_all: bool, keywords_only: bool):
     InsightGeneratorDBManager(
         database_context).update_reporting_tables(sector, year, generate_exp_sector_insights, generate_int_sector_insights, generate_mit_sector_insights, update_all, keywords_only)
 
 
-def update_chart_tables(database_context, generate_top10_exposure_chart_data: bool, generate_triangulation_data: bool, generate_yoy_chart_data:bool):
+def update_chart_tables(database_context, generate_top10_exposure_chart_data: bool, generate_triangulation_data: bool, generate_yoy_chart_data: bool):
     InsightGeneratorDBManager(
         database_context).update_chart_tables(generate_top10_exposure_chart_data, generate_triangulation_data, generate_yoy_chart_data)
-
 
 
 def get_sector_list(database_context):
@@ -243,7 +253,7 @@ def get_sector_list(database_context):
 def get_year_list(database_context):
     return InsightGeneratorDBManager(
         database_context).get_year_list()
-    
+
 
 # sector_list = get_sector_list('Test')
 # print(sector_list)
